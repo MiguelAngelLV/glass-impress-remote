96812aa1be227b7c766c231f132b386915bd1d6a
Miguel Ángel <miguelangellopez@realtracksystems.com>
2015-03-11 11:18:54 +0100

    Actualizado el Adapter para los servidores

--- app/src/main/java/com/mavl/glassimpressremote/adapters/ServersAdapter.java
+++ app/src/main/java/com/mavl/glassimpressremote/adapters/ServersAdapter.java
@@ -1,31 +1,37 @@
 package com.mavl.glassimpressremote.adapters;
 
+import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.glass.widget.CardBuilder;
 import com.google.android.glass.widget.CardScrollAdapter;
 
+import org.libreoffice.impressremote.communication.CommunicationService;
 import org.libreoffice.impressremote.communication.Server;
 import org.libreoffice.impressremote.util.Intents;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ServersAdapter extends CardScrollAdapter {
+public class ServersAdapter extends CardScrollAdapter implements ServiceConnection{
 
-    private ArrayList<Server> mServers;
+    private Context mContext;
+    private List<Server> mServers;
+    private CommunicationService mService;
 
-
-    public ServersAdapter() {
+    public ServersAdapter(Context context) {
+        mContext = context;
         mServers = new ArrayList<>();
     }
 
-    public void addServer(Server server) {
-        mServers.add(server);
-        notifyDataSetChanged();
-    }
 
     @Override
     public int getCount() {
@@ -39,17 +45,27 @@ public class ServersAdapter extends CardScrollAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        CardBuilder builder = new CardBuilder(mContext, CardBuilder.Layout.TEXT_FIXED);
+        builder.setText(mServers.get(i).getName() + "\n" + mServers.get(i).getAddress());
+        return builder.getView(view, viewGroup);
     }
 
     @Override
     public int getPosition(Object o) {
+        return mServers.indexOf(o);
+    }
 
-        for (int i = 0; i < mServers.size(); i++)
-            if (mServers.get(i) == o)
-                return i;
 
-        return -1;
+    private void configureService(Context context) {
+        //Conectamos con el servicio
+        Intent aServiceIntent = Intents.buildCommunicationServiceIntent(context);
+        context.startService(aServiceIntent);
+        context.bindService(aServiceIntent, this, Context.BIND_AUTO_CREATE);
+
+        //Añadimos los eventos que queremos recibir
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intents.Actions.SERVERS_LIST_CHANGED);
+        context.registerReceiver(mEvents, filter);
 
     }
 
@@ -57,10 +73,29 @@ public class ServersAdapter extends CardScrollAdapter {
     private BroadcastReceiver mEvents = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (Intents.Actions.SERVERS_LIST_CHANGED.equals(intent.getAction()))
+            if (mService == null)
+                return;
 
+            if (!Intents.Actions.SERVERS_LIST_CHANGED.equals(intent.getAction()))
+                return;
+
+            mServers = mService.getServers();
+            notifyDataSetChanged();
         }
     };
 
 
-}
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        CommunicationService.ServiceBinder aServiceBinder = (CommunicationService.ServiceBinder) service;
+        mService = aServiceBinder.getService();
+        mService.startServersSearch();
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        mService = null;
+    }
+
+
+ }
